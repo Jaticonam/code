617134a66d387fb118f8fb062b5fdd0a2d110f47
Julio Ticona <julioticonamaron@gmail.com>
2026-06-09 08:15:45 -0500

    chore: organize integration scripts and cleanup obsolete services

--- src/modules/integrations/services/generateMetaFeed.ts
+++
@@ -1,7 +0,0 @@
-import { loadAllProducts } from "@/modules/catalog/services/fetchProducts";
-import { exportMetaCsv } from "@/modules/integrations/connectors/meta/exporter";
-
-export const generateMetaFeedFromProducts = async () => {
-  const products = await loadAllProducts();
-  return exportMetaCsv(products);
-};
