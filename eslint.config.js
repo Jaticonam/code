ef7fafd74c26df64fcc256042ace4c0132d5468d
Julio Ticona <julioticonamaron@gmail.com>
2026-07-26 08:28:28 -0500

    chore(repo): excluye históricos autorizados de ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,21 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  {
+    ignores: [
+      "dist",
+
+      // Históricos conservados temporalmente fuera del tooling activo.
+      "BACKUP_PRODUCT_DETAIL_BLOCK_0_20260725_205950/**",
+      "BACKUP_PRODUCT_STATUS_20260725_193549/**",
+      "BACKUP_PRODUCT_STATUS_20260725_194057/**",
+      "BACKUP_PRODUCT_STATUS_CHANNELS_20260725_200316/**",
+      "BACKUP_PRODUCT_STATUS_UI_CART_20260725_194842/**",
+      "BlueprintBackups/**",
+      "TierDesignSystemBackup_20260721_090122/**",
+      "TierDesignSystemBackup_20260721_092019/**",
+    ],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
